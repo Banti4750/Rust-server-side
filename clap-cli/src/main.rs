852a6bbefb85552cb70cc347028485fedbf10c74
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-01 03:44:45 +0530

    feat: refactor main function to support async and add Nickel server

--- clap-cli/src/main.rs
+++ clap-cli/src/main.rs
@@ -1,17 +1,39 @@
 use clap::{Arg, Command};
+#[macro_use]
+extern crate nickel;
 
-fn main() {
+use nickel::Nickel;
+
+fn say_hello() -> &'static str {
+    "Hello dear world!"
+}
+#[tokio::main]
+async fn main() {
     let matches = Command::new("clap-cli")
         .version("0.1.0")
         .about("This is a test CLI")
         .arg(Arg::new("name").help("Your name").required(false).index(1))
+        .arg(Arg::new("greet").short('g').long("greet").help("Greet the user"))
         .get_matches();
 
-    if let Some(name) = matches.get_one::<String>("name") {
-        println!("Hello, {}!", name);
-    } else {
-        println!("Hello, world!");
-    }
+    // let nn = matches.get_one::<String>("name");
+    // let gerret = matches.get_flag("greet");
 
-    println!("CLI parsed successfully!");
+
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            println!("Received a request");
+        }
+    });
+
+     server.utilize(router! {
+        get "/get" => |_req, _res| {
+            say_hello()
+        }
+    });
+
+    println!("Starting server on http://127.0.0.1:6767");
+    server.listen("127.0.0.1:6767").await.unwrap();
 }
